86b80e15bf6b18ebaa60fe6f659f35443719184b
Dave Bushnell <dybushnell@gmail.com>
2019-08-03 22:57:06 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here
+
+const var1 = 31;
+const var2 = 2;
+const multiply = var1 * var2;
+
+const random = Math.random() * 10;
+
+const mod = 19 % 5;
+
+const max = Math.max(1,20);
