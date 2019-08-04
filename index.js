38a65fe23070f1582fdbc7fade940c4ea513da78
thenatural86 <philip.smalls86@gmail.com>
2019-08-04 00:33:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here
+const var1 = 5
+const var2 = 12.4
+const multiply = var1 * var2;
+const random = Math.floor(Math.random() * 5) + 1;
+const mod = 20 % 8;
+const max = Math.max(18,19,20);
+
