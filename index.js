7d938687b811fc502c08014de4f98d14a52f6b8e
Ben Braunstein <benzbraunstein@gmail.com>
2019-08-03 21:33:57 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here
+let var1 = 62;
+let var2 = 1;
+
+let multiply = var1 * var2;
+
+let random = Math.floor(Math.random * 10);
+
+let mod = 10 % 6;
+
+let max = Math.max(20, 10, 11, 5);
